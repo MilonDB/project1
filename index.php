b29f118e899657fbf4437bba28514b51e932a86b
milondb <2087950@talnet.nl>
2020-09-06 15:03:42 +0200

    html voor de login toegevoegd.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+?>
+
+<html>
+
+<head>
+    <title>INLOGFORM</title>
+</head>
+
+<body>
+    <form id="login">
+        <input type="text" id="username" placeholder="Username" required><br>
+        <input type="password" id="password" placeholder="Password" required><br>
+        <input type="submit" value="login"><br>
+        <a href="lostpass.php">Wachtwoord vergeten?</a><br><br>
+        <a href="signup.php">Klik hier om te registreren als u nog geen account heeft.</a>
+    </form>
+
+</body>
+
+</html>
